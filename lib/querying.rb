84fbedae8cd055e2ec3a3319becc616aa1a72bff
Huda Yousif <hjoski@gmail.com>
2019-11-06 14:41:47 -0800

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -49,7 +49,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
   FROM character_books
   INNER JOIN characters
-  ON characters.id = character_books.character_id
+  ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY COUNT(name) DESC;"
 end
